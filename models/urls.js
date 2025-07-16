41f2f2a8651f27407c18300b55eef7f424ce75f9
Rana Tashad <154610882+RanaTashad92@users.noreply.github.com>
2025-07-16 21:48:21 +0500

    Fix formatting in URL schema

--- models/urls.js
+++ models/urls.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const urlSchema = new mongoose.Schema({
    original_url: {
-      type: String,required: true
+      type: String,
+      required: true
    },
    short_code:{type:String,required:true,unique:true},
 
