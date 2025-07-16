c141cba1005b40f9e9dc858a081261f0faa8b353
Rana Tashad <154610882+RanaTashad92@users.noreply.github.com>
2025-07-16 21:41:43 +0500

    Fix mongoose connect syntax error in server.js file

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 const express =require ('express');
 const app = express();
 const mongoose =require ('mongoose');
-mongoose.connect(process.env.DATABASE_URL),{useNewUrlParser: true, useUnifiedTopology: true}
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 const db = mongoose.connection;
 db.on('error', (error) => console.error('Connection error:', error));
 db.once('open', () => console.log('Connected to Database'));
@@ -13,6 +16,8 @@ app.use(express.json());
 //Setting Routes
 const UrlRouter=require('./routes/urls');
 app.use('/urls', UrlRouter);
+
+
 app.listen(3000, () => {
     console.log('Server Started');
   });
